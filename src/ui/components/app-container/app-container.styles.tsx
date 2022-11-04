31b9c48376bc00a56b900ce36410c324189375f4
João Alberto <hey@joaoalberto.dev>
2022-11-03 21:46:54 -0300

    fix: app-container styles

--- src/ui/components/app-container/app-container.styles.tsx
+++ src/ui/components/app-container/app-container.styles.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components/native";
 const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.backgroundPrimary};
   flex: 1;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spacing.lg}px;
 `;
 
 export { Container };
