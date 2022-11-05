fb18d905e1eb3331740c280636ca5bc0a84a9a37
João Alberto <hey@joaoalberto.dev>
2022-11-05 13:08:19 -0300

    chore: removing unecessary view

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { View } from "react-native";
 import { AppContainer, Text, InsightsStack } from "@ui/components";
 import { useInsights } from "@hooks/use-insights";
 
@@ -8,13 +7,8 @@ export default function App() {
 
   return (
     <AppContainer>
-      <View>
-        <Text heading>How you doin’ 🦄</Text>
-        <InsightsStack
-          insights={insights}
-          onInsightPress={handleInsightCycle}
-        />
-      </View>
+      <Text heading>How you doin’ 🦄</Text>
+      <InsightsStack insights={insights} onInsightPress={handleInsightCycle} />
     </AppContainer>
   );
 }
