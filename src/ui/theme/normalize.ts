28891e3b1bacf6de58594b73a2b3ac0e3f17becb
João Alberto <hey@joaoalberto.dev>
2022-11-04 23:39:40 -0300

    fix: change constants names to uppercase

--- src/ui/theme/normalize.ts
+++ src/ui/theme/normalize.ts
@@ -2,11 +2,11 @@ import { Dimensions, PixelRatio } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SCREEN_HEIGHT = Dimensions.get("window").height;
-const scale = SCREEN_WIDTH / 375;
-const scaleHeight = SCREEN_HEIGHT / 812;
+const SCALE = SCREEN_WIDTH / 375;
+const SCALE_HEIGHT = SCREEN_HEIGHT / 812;
 
 const normalize = (size: number, forHeight?: boolean) => {
-  const newSize = size * (forHeight ? scaleHeight : scale);
+  const newSize = size * (forHeight ? SCALE_HEIGHT : SCALE);
 
   return Math.round(PixelRatio.roundToNearestPixel(newSize));
 };
